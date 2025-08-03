2eee191bffaa83ddee537c5da316c1118a8ee78c
ajmdrstalha <rstalha1@gmail.com>
2025-08-04 01:23:53 +0600

    modified:   app.py         modified:   requirements.txt         modified:   streamlit/config.toml

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import requests
 import streamlit as st
+from streamlit_option_menu import option_menu
 from streamlit_lottie import st_lottie
 from PIL import Image
 
-st.set_page_config(page_title="AJ MD. RS TALHA")
+st.set_page_config(
+    page_title="AJ MD. RS TALHA",
+    layout="wide"
+)
+#st.set_page_config(page_title="AJ MD. RS TALHA")
 
 def load_lottie_url(url: str):
     try:
@@ -14,7 +19,7 @@ def load_lottie_url(url: str):
     except:
         return None
 
-# ✅ Updated working Lottie link
+# Updated working Lottie link
 lottie_coding = load_lottie_url("https://assets2.lottiefiles.com/packages/lf20_0yfsb3a1.json")
 #image upload for project 1
 image1 = Image.open("images/project1.png")
@@ -25,30 +30,108 @@ image2 = Image.open("images/project2.png")
 # ----- Header Section ----
 st.subheader("Welcome to my portfolio")
 st.title("AJ MD RS TALHA")
-st.write("DevOps & Automation Engineer")
-url = "https://www.linkedin.com/in/ajmdrstalha/"
-st.write("Check out my [Linkedin](%s)" % url)
+st.write("DevOps & Network Automation Engineer")
 
-#------About Section ----
+
+# ----- Load CSS for Bootstrap Icons ----
+# LinkedIn URL
+linkedin_url = "https://www.linkedin.com/in/ajmdrstalha/"
+github_url = "https://github.com/ajmdrstalha"
+# Load Bootstrap Icons
+st.markdown(
+    """
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    """,
+    unsafe_allow_html=True,
+)
+
+# LinkedIn icon with hyperlink
+st.markdown(
+    f"""
+    <div style="display: flex; gap: 920px; align-items: center;">
+        <p>
+            <a href="{linkedin_url}" target="_blank" style="text-decoration: none;">
+                <i class="bi bi-linkedin" style="font-size: 20px; color: #0A66C2;"></i>
+                <span style="font-size: 18px; margin-left: 6px;">LinkedIn</span>
+            </a>
+        </p>
+        <p>
+            <a href="{github_url}" target="_blank" style="text-decoration: none;">
+                <i class="bi bi-github" style="font-size: 20px; color: #24292e;"></i>
+                <span style="font-size: 18px; margin-left: 6px; color: #FFFFFF;">GitHub</span>
+            </a>
+        </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# ----- Streamlit Style ----
+
+
+# ----- Option Menu ----
 with st.container():
-    st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.header("About Me")
-        st.write(
-            """
-            I am a DevOps Engineer with a passion for automating and optimizing processes. 
-            I have experience in cloud computing, CI/CD, and infrastructure as code. 
-            I am always eager to learn new technologies and improve my skills.
-            """
-        )
-    with right_column:
-        st_lottie(lottie_coding, height=300, key="coding")
+    selected = option_menu(
+        menu_title=None,
+        options=["About", "Skills", "Projects", "Contact"],
+        icons=["person-circle","1-circle" , "code-slash", "chat"],
+        orientation="horizontal",
+    )
+#------About Section ----
+if selected == "About":
+    with st.container():
+        st.write("---")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.header("About Me")
+            st.write(
+                """
+                I am a DevOps Engineer with a passion for automating and optimizing processes. 
+                I have experience in cloud computing, CI/CD, and infrastructure as code. 
+                I am always eager to learn new technologies and improve my skills.
+                """
+            )
+        with right_column:
+            st_lottie(lottie_coding, height=300, key="coding")
 
+
+# ----- Skills Section ----
+if selected == "Skills":
+    st.write("---")
+    st.header("Skills")
+    st.markdown(
+        """
+        <ul>
+        <ul style="list-style: none; padding-left: 0;">
+            <li>- Version Control (Git) <i class="bi bi-git" style="color:#f34f29; margin-left: 8px;"></i></li>
+            <li>- Linux (Ubuntu, RedHat) <i class="bi bi-ubuntu" style="color:#198754; margin-left: 8px;"></i></li>
+            <li>- Scripting & Automation (Python, Bash) <i class="bi bi-filetype-py" style="color:#3572A5; margin-left: 8px;"></i></li>
+            <li>- Networking (TCP/IP, DNS, DHCP) <i class="bi bi-hdd-network" style="color:#fd7e14; margin-left: 8px;"></i></li>
+            <li>- Containerization & Orchestration (Docker, Kubernetes) <i class="bi bi-box-seam" style="color:#0dcaf0; margin-left: 8px;"></i></li>
+            <li>- Cloud Computing (AWS) <i class="bi bi-cloud" style="color:#0A66C2; margin-left: 8px;"></i></li>
+            <li>- CI/CD (Jenkins, GitHub Actions) <i class="bi bi-arrow-repeat" style="color:#198754; margin-left: 8px;"></i></li>
+            <li>- Infrastructure as Code (Terraform, Ansible) <i class="bi bi-hdd-network-fill" style="color:#6f42c1; margin-left: 8px;"></i></li>
+            <li>- Monitoring & Logging (Prometheus, Grafana) <i class="bi bi-eyeglasses" style="color:#fd7e14; margin-left: 8px;"></i></li>
+        </ul>
+        """,
+        unsafe_allow_html=True
+    )
+    st.markdown(
+        """
+        -<ul>Soft Skills</ul>
+        <ul style="list-style: none; padding-left: 0;">
+            <li>- Problem Solving</li>
+            <li>- Team Collaboration</li>
+            <li>- Communication</li>
+            <li>- Adaptability</li>
+            <li>- Time Management</li>
+        """,
+        unsafe_allow_html=True
+    )
 
 # ----- Projects ----
-    
-with st.container():
+if selected == "Projects":
     st.write("---")
     st.header("Projects")
     st.write("##")
@@ -64,8 +147,7 @@ with st.container():
             """
         )
         st.markdown("[GitHub](https://github.com/ajmdrstalha/Epon-Command-Generator)")
-        
-        
+
     
     st.write("##")
     image_column, text_column = st.columns((1, 2))
